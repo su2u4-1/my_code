d5bd759e8de8547eedc645d4bb8f5ebb868e745d
su2u4-1 <Joey23.su@gmail.com>
2024-01-22 01:03:57 +0800

    202401220

--- python/還沒成功/game0.py
+++ python/還沒成功/game0.py
@@ -8,17 +8,21 @@ class Button:
         self.x = x
         self.y = y
         if chinese:
-            ntext = ""
-            for i in range(len(text)):
-                if 19968 <= int(ord(text[i])) <= 40959:
-                    ntext += text[i] + "\u200B"
-                else:
-                    ntext += text[i]
-            self.text = ntext
+            self.retext(text)
         else:
             self.text = text
-        self.w = len(self.text)
+            self.w = len(self.text)
         self.do = do
+
+    def retext(self, text: str):
+        ntext = ""
+        for i in range(len(text)):
+            if 19968 <= int(ord(text[i])) <= 40959:
+                ntext += text[i] + "\u0000"
+            else:
+                ntext += text[i]
+        self.text = ntext
+        self.w = len(self.text)
 
 
 class Surface:
@@ -41,29 +45,30 @@ class Surface:
                 else:
                     a.append(" ")
             self.s.append(a)
+        for b in self.b:
+            for i in range(0, b.w):
+                if b.x + i < self.w:
+                    self.s[b.y][b.x + i] = b.text[i]
+            if b == self.b[self.c]:
+                self.s[b.y][b.x - 1] += "\033[7m"
+                if b.x + b.w - 1 < self.w:
+                    self.s[b.y][b.x + b.w - 1] += "\033[0m"
 
-    def update(self, b: Button):
-        for i in range(0, b.w):
-            if b.x + i < self.w:
-                self.s[b.y][b.x + i] = b.text[i]
-        if b == self.b[self.c]:
-            self.s[b.y][b.x - 1] += "\033[7m"
-            if b.x + b.w - 1 < self.w:
-                self.s[b.y][b.x + b.w - 1] += "\033[0m"
-
-    def display(self):
+    def update(self, readkey: bool = True):
         self.refresh()
-        for i in self.b:
-            self.update(i)
         system("cls")
         for y in range(self.h):
             for x in range(self.w):
                 if self.s[y][x] == " " and self.f:
                     print(str(x)[-1], end="")
+                elif y == 0 and self.f and x < len(self.n):
+                    print(self.n[x], end="")
                 else:
                     print(self.s[y][x], end="")
             print()
-        sleep(0.2)
+        if readkey:
+            sleep(0.3)
+            return read_key(True)
 
 
 def move(key: str, s: Surface):
@@ -90,51 +95,36 @@ def move(key: str, s: Surface):
     return en
 
 
-def newgame():
-    s = Surface(W, H, "name")
-    s.b.append(Button(round(W / 2), round(H / 2), "確認", True, "surface=main()"))
+def main():
+    s = Surface(W, H, "main")
+    s.b.append(Button(round(W / 2), round(H / 2), f"還沒做好", True, "print(name)\nexit()"))
     return s
 
 
-def main():
-    s = Surface(W, H, "main")
-    s.b.append(Button(round(W / 2), round(H / 2), "還沒做好", True, exit))
+def start():
+    s = Surface(W, H, "start")
+    s.b.append(Button(round(W / 3) - 4, round(H / 3), "開新遊戲", True, "system('cls')\nname=input('請輸入名字:')\nsurface=main()"))
+    s.b.append(Button(round(W / 3) - 4, round(H / 3 * 2) - 1, "讀取存檔", True))
+    s.b.append(Button(round(W / 3 * 2) - 4, round(H / 3), "儲存遊戲", True))
+    s.b.append(Button(round(W / 3 * 2) - 4, round(H / 3 * 2) - 1, "離開遊戲", True, "exit()"))
     return s
 
 
 W, H = 120, 30
 name = "player"
-surface = Surface(W, H, "start")
-surface.b.append(Button(round(W / 3) - 4, round(H / 3), "開新遊戲", True, "surface=newgame()"))
-surface.b.append(Button(round(W / 3) - 4, round(H / 3 * 2) - 1, "讀取存檔", True))
-surface.b.append(Button(round(W / 3 * 2) - 4, round(H / 3), "儲存遊戲", True))
-surface.b.append(Button(round(W / 3 * 2) - 4, round(H / 3 * 2) - 1, "離開遊戲", True, exit))
-surface.display()
+surface = start()
+key = surface.update()
 while True:
-    key = read_key()
-    if surface.n == "name":
-        surface.display()
-        if key == "enter":
-            name = surface.b[0].text[4:]
-        elif key == "backspace":
-            surface.b[0].text = surface.b[0].text[:-1]
-            surface.b[0].w -= 1
-        else:
-            surface.b[0].text += key
-            surface.b[0].w += 1
     if key == "w" or key == "a" or key == "s" or key == "d":
         surface.c = move(key, surface)
-        surface.display()
+        key = surface.update()
     elif key == "enter":
         do = surface.b[surface.c].do
-        if type(do) == str:
-            exec(do)
-        else:
-            do()
-        surface.display()
+        exec(do)
+        key = surface.update()
     elif key == "f":
         if surface.f:
             surface.f = False
         else:
             surface.f = True
-        surface.display()
+        key = surface.update()
