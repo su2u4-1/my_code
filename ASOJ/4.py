63a32ad76c8017f05bfd046cdc35fcde3f47d19a
su2u4 <Joey23.su@gmail.com>
2024-03-30 23:35:19 +0800

    202403301

--- ASOJ/4.py
+++ ASOJ/4.py
@@ -1,9 +1,16 @@
+def mul(p, n, k):
+    q = [0] * (n + 1)
+    q[0] = p[0]
+    for i in range(1, k + 1):
+        q[i] = q[i - 1] + p[i]
+    for i in range(k + 1, n + 1):
+        q[i] = q[i - 1] + p[i] - p[i - k - 1]
+    return q
+
+
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-for i in range(1, n + 1):
-    if i == 1:
-        print(len(a) % 998244353, end=" ")
-    elif sum(a) < i:
-        print(0, end=" ")
-    else:
-        pass
+a = map(int, input().split())
+p = [1] + [0] * n
+for i in a:
+    p = mul(p, n, i)
+print(*p[1:])
