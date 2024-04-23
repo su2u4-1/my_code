e201e65103b7027daf12032fde929878d5658444
su2u4 <Joey23.su@gmail.com>
2024-04-23 22:54:38 +0800

    update

---
+++ python/.py
@@ -0,0 +1,12 @@
+#b938 MLE 50%
+n, m = map(int, input().split())
+pl = [i + 1 for i in range(n)]
+for i in map(int, input().split()):
+    if i in pl:
+        ix = pl.index(i)
+        if ix == len(pl) - 1:
+            print("0u0 ...... ?")
+        else:
+            print(pl.pop(ix + 1))
+    else:
+        print("0u0 ...... ?")
