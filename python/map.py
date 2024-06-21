4e5a0c644da2430875a948a7858a34c20d713b59
su2u4 <Joey23.su@gmail.com>
2024-06-22 01:55:51 +0800

    .

--- python/map.py
+++ python/map.py
@@ -9,17 +9,21 @@ font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 
 point: list[tuple[int, int, tuple[int, int, int]]] = []
+screen.fill((0, 0, 0))
 
 
-def dist(x: int, y: int) -> tuple[int, int, int] | None:
-    min_d = float("inf")
-    color = None
-    for i in point:
-        d = (x - i[0]) ** 2 + (y - i[1]) ** 2
-        if min_d > d:
-            min_d = d
-            color = i[2]
-    return color
+def draw() -> None:
+    screen.fill((0, 0, 0))
+    for x in range(W):
+        for y in range(H):
+            min_d = W * H
+            color = (0, 0, 0)
+            for p in point:
+                d = (x - p[0]) ** 2 + (y - p[1]) ** 2
+                if d < min_d:
+                    min_d = d
+                    color = p[2]
+            screen.set_at((x, y), color)
 
 
 while True:
@@ -33,14 +37,12 @@ while True:
                 exit()
             elif event.key == pygame.K_SPACE:
                 point.append((rr(0, W), rr(0, H), (rr(0, 256), rr(0, 256), rr(0, 256))))
-
-    for i in range(W):
-        for j in range(H):
-            color = dist(i, j)
-            if color is None:
-                screen.set_at((i, j), (0, 0, 0))
-            else:
-                screen.set_at((i, j), color)
+                draw()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                mx, my = pygame.mouse.get_pos()
+                point.append((mx, my, (rr(0, 256), rr(0, 256), rr(0, 256))))
+                draw()
 
     for i in point:
         pygame.draw.circle(screen, (0, 0, 0), (i[0], i[1]), 5, 2)
