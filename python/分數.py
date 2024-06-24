46ce4b426f14b935a9a6c4cb227417cd08919529
su2u4 <70791378+su2u4-1@users.noreply.github.com>
2024-06-24 18:36:54 +0800

    Update 分數.py

--- python/分數.py
+++ python/分數.py
@@ -1,20 +1,38 @@
 from typing import Any, Self, Iterable, Optional, Literal, Mapping
 
 class Fraction:
-    def __init__(self, *args):
-        if len(args) == 1:
-            if type(args[0]) == float:
-                pass
-            elif type(args[0]) == int:
-                pass
-            elif type(args[0]) == Fraction:
-                pass
-            else:
-                try:
-                    int(args[0])
-                except:
-                    raise TypeError()
-        elif len(args) == 2:
-            pass
-        else:
-            raise TypeError()
+    def __init__(self, numerator, denominator=1):
+        if type(numerator) == float:
+            while numerator - int(numerator) != 0:
+                denominator *= 10
+                numerator *= 10
+            numerator = int(numerator)
+        elif type(numerator) == Fraction:
+            denominator *= numerator.denominator
+            numerator = numerator.numerator
+        elif type(numerator) != int:
+            try:
+                numerator = int(numerator)
+            except:
+                raise TypeError()
+
+        if type(denominator) == float:
+            while denominator - int(denominator) != 0:
+                denominator *= 10
+                numerator *= 10
+            denominator = int(denominator)
+        elif type(denominator) == Fraction:
+            numerator *= denominator.denominator
+            denominator = denominator.numerator
+        elif type(denominator) != int:
+            try:
+                denominator = int(denominator)
+            except:
+                raise TypeError()
+        self.approx()
+
+    def approx(self):
+        for i in range(2, min(self.numerator, self.denominator)+1):
+            if self.numerator % i == 0 and self.denominator % i == 0:
+                self.denominator /= i
+                self.numerator /= i
