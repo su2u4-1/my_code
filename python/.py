84f5f18a1225ee97c9afa2207e68b0cef6f14bde
su2u4-1 <joey23.su@gmail.com>
2024-06-25 16:00:19 +0800

    test

---
+++ python/.py
@@ -0,0 +1,43 @@
+from math import trunc, floor, ceil
+from 分數 import Fraction
+
+a = "Fraction(3, 2)"
+aa = 1.5
+b = "Fraction(3, 4)"
+ba = 0.75
+c = 2
+d = 2.5
+tq = [a, b, c, d]
+ta = [aa, ba, c, d]
+
+
+t2 = ["print({0}+{1})",
+"print({0}-{1})",
+"print({0}*{1})",
+"print({0}/{1})",
+"print({0}//{1})",
+"print({0}%{1})",
+"print(divmod({0}, {1}))",
+"print({0}**{1})",
+"print({0}-{1})",
+"print({0}+{1})",
+"print({0}<{1})",
+"print({0}<={1})",
+"print({0}=={1})",
+"print({0}!={1})",
+"print({0}>{1})",
+"print({0}>={1})"]
+
+t1 = [
+"print(abs({0}))",
+"print(complex({0}))",
+"print(int({0}))",
+"print(float({0}))",
+"print(round({0}))",
+"print(trunc({0}))",
+"print(floor({0}))",
+"print(ceil({0}))",
+"print(str({0}))",
+"print(repr({0}))",
+"print('{{0}}'.format({0}))",
+"print(hash({0}))"]
