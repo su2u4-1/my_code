a27406afb21d9e34693a6157cabbc1efdd8bd5b4
su2u4 <Joey23.su@gmail.com>
2024-08-22 14:31:59 +0800

    2048 is almost complete

--- python_game/2048.py
+++ python_game/2048.py
@@ -1,78 +1,131 @@
+from keyboard import read_key
+from os import system
 from random import randrange, choices
+from sys import stdin, stdout
+from time import sleep
+
+# from textgame import Game
 
 
-def check(m: list[list[int]]) -> bool:
-    for i in m:
-        if 0 in i:
-            return False
-    for i in range(R):
-        for j in range(R):
-            for k in range(4):
-                if 0 <= i + D[k][0] < R and 0 <= j + D[k][1] < R and m[i][j] == m[i + D[k][0]][j + D[k][0]]:
-                    return False
-    return True
+# class Game2048(Game):
+class Game2048:
+    def __init__(self, r: int) -> None:
+        self.init(r)
 
+    def init(self, r: int) -> None:
+        self.D = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        self.R = r
+        self.m = [[0 for _ in range(r)] for _ in range(r)]
+        self.add()
 
-def add(m: list[list[int]]) -> None:
-    while True:
-        x, y = randrange(R), randrange(R)
-        if m[x][y] == 0:
-            m[x][y] = choices((2, 4), (4, 1))[0]
-            return
+    def check(self) -> int:
+        for i in self.m:
+            if 0 in i:
+                return 0
+        for i in range(self.R):
+            for j in range(self.R):
+                for k in range(4):
+                    if 0 <= i + self.D[k][0] < self.R and 0 <= j + self.D[k][1] < self.R:
+                        if self.m[i][j] == self.m[i + self.D[k][0]][j + self.D[k][1]]:
+                            return 1
+        return 2
 
+    def add(self) -> None:
+        while True:
+            x, y = randrange(self.R), randrange(self.R)
+            if self.m[x][y] == 0:
+                self.m[x][y] = choices((2, 4), (4, 1))[0]
+                return
 
-def show(m: list[list[int]]) -> None:
-    str_m = [[str(j) for j in i] for i in m]
-    long = 1
-    for i in str_m:
-        for j in range(R):
-            long = max(long, len(i[j]))
-    for i in m:
-        for j in range(R):
-            if i[j] == 0:
-                print(f"|{" "*long}|", end="")
+    def show(self) -> None:
+        system("cls")
+        str_m = [[str(j) for j in i] for i in self.m]
+        long = 1
+        for i in str_m:
+            for j in range(self.R):
+                long = max(long, len(i[j]))
+        for i in self.m:
+            for j in range(self.R):
+                if i[j] == 0:
+                    print(f"|{" "*long}|", end="")
+                else:
+                    print(f"|{i[j]:{long}}|", end="")
+            print()
+
+    def move(self, range1: range, range2: range, d: int) -> int:
+        for i in range1:
+            for j in range2:
+                if self.m[i][j] == 0:
+                    continue
+                now_i, now_j = i, j
+                while True:
+                    if self.m[now_i + self.D[d][0]][now_j + self.D[d][1]] == 0:
+                        self.m[now_i + self.D[d][0]][now_j + self.D[d][1]] = self.m[now_i][now_j]
+                        self.m[now_i][now_j] = 0
+                    elif self.m[now_i][now_j] == self.m[now_i + self.D[d][0]][now_j + self.D[d][1]]:
+                        self.m[now_i][now_j] = 0
+                        self.m[now_i + self.D[d][0]][now_j + self.D[d][1]] *= 2
+                        break
+                    now_i, now_j = now_i + self.D[d][0], now_j + self.D[d][1]
+                    if now_i + self.D[d][0] < 0 or now_i + self.D[d][0] >= self.R or now_j + self.D[d][1] < 0 or now_j + self.D[d][1] >= self.R:
+                        break
+        return self.check()
+
+    def main(self) -> None:
+        d = ""
+        t = 0
+        while True:
+            if d == "w" or d == "up":
+                t = self.move(range(1, self.R), range(self.R), 0)
+            elif d == "a" or d == "left":
+                t = self.move(range(self.R), range(1, self.R), 1)
+            elif d == "s" or d == "down":
+                t = self.move(range(self.R - 2, -1, -1), range(self.R), 2)
+            elif d == "d" or d == "right":
+                t = self.move(range(self.R), range(self.R - 2, -1, -1), 3)
+            if t == 2:
+                print("game over")
+                break
+            elif t == 0:
+                self.add()
+            self.show()
+            sleep(0.2)
+            d = read_key()
+        self.again()
+
+    def again(self) -> None:
+        stdin.flush()
+        stdout.flush()
+        o = input("again?[y/n]: ")
+        if o in ("Y", "y", "Yes", "yes"):
+            o = input("change size?[y/n]: ")
+            if o in ("Y", "y", "Yes", "yes"):
+                while True:
+                    s = input("size: ")
+                    try:
+                        s = int(s)
+                        if s > 1:
+                            break
+                        else:
+                            print("input error")
+                    except:
+                        print("input error")
             else:
-                print(f"|{i[j]:{long}}|", end="")
-        print()
+                s = self.R
+            self.init(s)
+            self.main()
 
 
-def move(range1: range, range2: range, d: int) -> None:
-    for i in range1:
-        for j in range2:
-            if m[i][j] == 0:
-                continue
-            now_i, now_j = i, j
-            while True:
-                if m[now_i + D[d][0]][now_j + D[d][1]] == 0:
-                    m[now_i + D[d][0]][now_j + D[d][1]] = m[now_i][now_j]
-                    m[now_i][now_j] = 0
-                elif m[now_i][now_j] == m[now_i + D[d][0]][now_j + D[d][1]]:
-                    m[now_i][now_j] = 0
-                    m[now_i + D[d][0]][now_j + D[d][1]] *= 2
-                    break
-                now_i, now_j = now_i + D[d][0], now_j + D[d][1]
-                if now_i + D[d][0] < 0 or now_i + D[d][0] >= R or now_j + D[d][1] < 0 or now_j + D[d][1] >= R:
-                    break
-
-
-D = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-R = 4
-m = [[0 for _ in range(R)] for _ in range(R)]
-d = ""
-add(m)
-add(m)
-
-while True:
-    if d == "w":
-        move(range(1, R), range(R), 0)
-    elif d == "a":
-        move(range(R), range(1, R), 1)
-    elif d == "s":
-        move(range(R - 2, -1, -1), range(R), 2)
-    elif d == "d":
-        move(range(R), range(R - 2, -1, -1), 3)
-    if d == "w" or d == "a" or d == "s" or d == "d":
-        if add(m):
-            break
-    show(m)
-    d = input("wasd: ")
+if __name__ == "__main__":
+    while True:
+        s = input("size: ")
+        try:
+            s = int(s)
+            if s > 1:
+                break
+            else:
+                print("input error")
+        except:
+            print("input error")
+    game = Game2048(s)
+    game.main()
