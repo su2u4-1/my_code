cce45e0ef7d4d573174df5a461e2411c5469b275
su2u4 <Joey23.su@gmail.com>
2024-11-25 22:03:27 +0800

    update pull.py

--- python/pull.py
+++ python/pull.py
@@ -1,13 +1,17 @@
 from os import listdir, system, chdir
 from os.path import isdir, join
 from subprocess import getoutput
+from sys import argv
 
 rootdir = "D:\\"
+if len(argv) >= 1:
+    if isdir(argv[0]):
+        rootdir = argv[0]
 
 for i in listdir(rootdir):
     i = join(rootdir, i)
     if isdir(i):
-        print(f"\n檢查資料夾：{i}")
+        print(f"\nChecking folder: {i}")
         try:
             if ".git" in listdir(i):
                 chdir(i)
@@ -15,15 +19,15 @@ for i in listdir(rootdir):
                 if len(status) == 0:
                     status = getoutput("git show-branch --remote")
                     if status == "No revs to be shown.":
-                        print("    無遠端儲存庫，略過")
+                        print("    No remote repository, skipping")
                     else:
-                        print("    無未提交的更改，執行 git pull...")
+                        print("    No uncommitted changes, running git pull...")
                         system("git pull --all")
                 else:
-                    print("    未提交的變更:")
+                    print("    Uncommitted changes:")
                     for f in status.split("\n"):
                         print("       ", f)
             else:
-                print("    這不是一個 Git 資料夾，略過")
+                print("    Not a Git repository, skipping")
         except PermissionError:
-            print("    存取被拒，略過")
+            print("    Access denied, skipping")
