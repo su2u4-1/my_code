88c9f1db01d9fa374338aa4fefba1c4b3a4269a3
su2u4 <Joey23.su@gmail.com>
2024-11-26 21:40:53 +0800

    update pull.py

--- python/pull.py
+++ python/pull.py
@@ -3,33 +3,42 @@ from os.path import isdir, join
 from subprocess import getoutput
 from sys import argv
 
-rootdir = "D:\\"
-if len(argv) >= 1:
+
+def check(dir: str):
+    print(f"\nChecking folder: {dir}")
+    try:
+        if ".git" in listdir(dir):
+            chdir(dir)
+            status = getoutput("git status --porcelain")
+            if len(status) == 0:
+                status = getoutput("git show-branch --remote")
+                if status == "No revs to be shown.":
+                    print("    No remote repository, skipping")
+                else:
+                    print("    No uncommitted changes, running git pull...")
+                    system("git pull --all")
+            else:
+                print("    Uncommitted changes:")
+                for f in status.split("\n"):
+                    print("       ", f)
+        else:
+            print("    Not a Git repository, skipping")
+    except PermissionError:
+        print("    Access denied, skipping")
+    except Exception as e:
+        print(f"    Error: {e}, skipping")
+
+
+def main(dir: str):
+    check(dir)
+    for i in listdir(dir):
+        if isdir(join(dir, i)):
+            main(join(dir, i))
+
+
+rootdir = ".\\"
+if len(argv) >= 2:
     if isdir(argv[1]):
         rootdir = argv[1]
 
-for i in listdir(rootdir):
-    i = join(rootdir, i)
-    if isdir(i):
-        print(f"\nChecking folder: {i}")
-        try:
-            if ".git" in listdir(i):
-                chdir(i)
-                status = getoutput("git status --porcelain")
-                if len(status) == 0:
-                    status = getoutput("git show-branch --remote")
-                    if status == "No revs to be shown.":
-                        print("    No remote repository, skipping")
-                    else:
-                        print("    No uncommitted changes, running git pull...")
-                        system("git pull --all")
-                else:
-                    print("    Uncommitted changes:")
-                    for f in status.split("\n"):
-                        print("       ", f)
-            else:
-                print("    Not a Git repository, skipping")
-        except PermissionError:
-            print("    Access denied, skipping")
-        except Exception as e:
-            print(f"    Error: {e}, skipping")
+main(rootdir)
