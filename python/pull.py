dd8382b2326b256f6b230cc8dbbdc2a3ef114ad4
su2u4 <Joey23.su@gmail.com>
2024-12-01 23:44:02 +0800

    update pull.py

--- python/pull.py
+++ python/pull.py
@@ -1,11 +1,12 @@
-from os import listdir, system, chdir
+from os import listdir, system, chdir, getcwd
 from os.path import isdir, join
 from subprocess import getoutput
 from sys import argv
 
 
-def check(dir: str):
+def check(dir: str) -> bool:
     print(f"\nChecking folder: {dir}")
+    state = True
     try:
         if ".git" in listdir(dir):
             chdir(dir)
@@ -17,28 +18,42 @@ def check(dir: str):
                 else:
                     print("    No uncommitted changes, running git pull...")
                     system("git pull --all")
+                    state = False
             else:
                 print("    Uncommitted changes:")
                 for f in status.split("\n"):
                     print("       ", f)
+                state = False
         else:
             print("    Not a Git repository, skipping")
     except PermissionError:
         print("    Access denied, skipping")
+        state = False
     except Exception as e:
         print(f"    Error: {e}, skipping")
+        state = False
+    return state
 
 
-def main(dir: str):
-    check(dir)
+def recursion(dir: str) -> None:
+    state = check(dir)
+    if state:
+        for i in listdir(dir):
+            if isdir(join(dir, i)):
+                recursion(join(dir, i))
+
+
+def norecursion(dir: str) -> None:
     for i in listdir(dir):
-        if isdir(join(dir, i)):
-            main(join(dir, i))
+        if i != ".git" and isdir(join(dir, i)):
+            check(join(dir, i))
 
 
-rootdir = ".\\"
-if len(argv) >= 2:
-    if isdir(argv[1]):
-        rootdir = argv[1]
+rootdir = getcwd()
+if len(argv) >= 2 and isdir(argv[1]) and "def" not in argv[1]:
+    rootdir = argv[1]
 
-main(rootdir)
+if len(argv) >= 3 and "-r" in argv[2]:
+    recursion(rootdir)
+else:
+    norecursion(rootdir)
