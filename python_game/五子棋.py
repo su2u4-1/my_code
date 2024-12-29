4b4ec1e25fdbcc29e1bf0753ab9a97335c8e7cd8
su2u4 <Joey23.su@gmail.com>
2024-12-30 02:47:13 +0800

    五子棋AI

--- python_game/五子棋.py
+++ python_game/五子棋.py
@@ -1,8 +1,9 @@
 from typing import Literal
+from random import choice, randint
 
 import pygame
 
-from Game import get_int, D8
+from gamelib import get_int, D8
 
 
 class Game_gomoku:
@@ -24,6 +25,9 @@ class Game_gomoku:
         self.chessBoard = [[0 for _ in range(size)] for _ in range(size)]
         self.run = True
         self.result = (False, ((-1, -1), (-1, -1)))
+        self.ai_side: Literal["black", "white", "no"] = "no"
+        self.temp_text = ""
+        self.temp_text_time = 0
 
     def show(self) -> None:
         self.screen.fill((238, 154, 73))
@@ -49,16 +53,28 @@ class Game_gomoku:
         self.screen.blit(self.font.render("reset", True, (0, 0, 0)), (5, self.h + 5))
         pygame.draw.rect(self.screen, (0, 0, 0), (60, self.h, 60, 30), 5)
         self.screen.blit(self.font.render("exit", True, (0, 0, 0)), (70, self.h + 5))
+        if self.ai_side == "no":
+            pygame.draw.rect(self.screen, (0, 0, 0), (120, self.h, 60, 30), 5)
+            self.screen.blit(self.font.render("AI", True, (0, 0, 0)), (140, self.h + 5))
         mx, my = pygame.mouse.get_pos()
-        if 60 <= mx < 120 and self.h <= my < self.h + 30:
-            pygame.draw.rect(self.screen, (255, 255, 255), (60, self.h, 60, 30), 5)
-        elif 0 <= mx < 60 and self.h <= my < self.h + 30:
+        if 0 <= mx < 60 and self.h <= my < self.h + 30:
             pygame.draw.rect(self.screen, (255, 255, 255), (0, self.h, 60, 30), 5)
+        elif 60 <= mx < 120 and self.h <= my < self.h + 30:
+            pygame.draw.rect(self.screen, (255, 255, 255), (60, self.h, 60, 30), 5)
+        elif 120 <= mx < 180 and self.h <= my < self.h + 30 and self.ai_side == "no":
+            pygame.draw.rect(self.screen, (255, 255, 255), (120, self.h, 60, 30), 5)
+        if self.temp_text != "":
+            self.temp_text_time -= 1
+            if self.temp_text_time == 0:
+                self.temp_text = ""
         if self.status == "gamerun":
-            text = self.font.render(f"({self.mx},{self.my})  {self.turn}", True, (0, 0, 0))
+            text = self.font.render(f"({self.mx},{self.my}) {self.turn} {self.temp_text}", True, (0, 0, 0))
         else:
-            text = self.font.render(f"({self.mx},{self.my})  {self.status}", True, (0, 0, 0))
-        self.screen.blit(text, (125, self.h + 5))
+            text = self.font.render(f"({self.mx},{self.my}) {self.status} {self.temp_text}", True, (0, 0, 0))
+        if self.ai_side == "no":
+            self.screen.blit(text, (185, self.h + 5))
+        else:
+            self.screen.blit(text, (125, self.h + 5))
 
     def check(self) -> tuple[bool, tuple[tuple[int, int], tuple[int, int]]]:
         n = 0
@@ -71,7 +87,7 @@ class Game_gomoku:
                         x1 = x
                         y1 = y
                         for _ in range(4):
-                            if x1 + D8[i][0] < 0 or x1 + D8[i][0] > 14 or y1 + D8[i][1] < 0 or y1 + D8[i][1] > 14:
+                            if x1 + D8[i][0] < 0 or x1 + D8[i][0] > self.size - 1 or y1 + D8[i][1] < 0 or y1 + D8[i][1] > self.size - 1:
                                 break
                             elif self.chessBoard[x1 + D8[i][0]][y1 + D8[i][1]] != self.chessBoard[x][y]:
                                 break
@@ -86,6 +102,63 @@ class Game_gomoku:
         if n == 0:
             self.status = "draw"
         return False, ((-1, -1), (-1, -1))
+
+    def put_chess(self) -> None:
+        if self.chessBoard[self.mx][self.my] == 0 and self.status == "gamerun":
+            if self.turn == "black":
+                self.turn = "white"
+                self.chessBoard[self.mx][self.my] = 1
+            else:
+                self.turn = "black"
+                self.chessBoard[self.mx][self.my] = 2
+            if self.turn == self.ai_side:
+                self.ai()
+        else:
+            raise RuntimeError("position is not empty or game is over")
+
+    def ai(self) -> None:
+        possible_pos: list[list[tuple[int, int]]] = [[], [], [], [], [], [], [], []]
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.chessBoard[x][y] != 0:
+                    s = 0
+                    if ("no", "black", "white")[self.chessBoard[x][y]] == self.ai_side:
+                        s = 1
+                    for i in range(8):
+                        l = 0
+                        d = (-1, -1)
+                        x1 = x
+                        y1 = y
+                        for j in range(3):
+                            if x1 + D8[i][0] < 0 or x1 + D8[i][0] > self.size - 1 or y1 + D8[i][1] < 0 or y1 + D8[i][1] > self.size - 1:
+                                break
+                            elif self.chessBoard[x1 + D8[i][0]][y1 + D8[i][1]] != self.chessBoard[x][y]:
+                                if self.chessBoard[x1 + D8[i][0]][y1 + D8[i][1]] == 0:
+                                    l = j + 1
+                                    d = (x1 + D8[i][0], y1 + D8[i][1])
+                                break
+                            x1 += D8[i][0]
+                            y1 += D8[i][1]
+                        else:
+                            l = 4
+                        if l > 0:
+                            if s == 0:
+                                possible_pos[l + 3].append(d)
+                            else:
+                                possible_pos[l - 1].append(d)
+        for i in range(8):
+            if len(possible_pos[7 - i]) > 0:
+                x, y = choice(possible_pos[7 - i])
+                if self.chessBoard[x][y] == 0:
+                    self.mx, self.my = x, y
+                    self.put_chess()
+                    return
+        while True:
+            x, y = randint(0, self.size - 1), randint(0, self.size - 1)
+            if self.chessBoard[x][y] == 0:
+                self.mx, self.my = x, y
+                self.put_chess()
+                return
 
     def main(self) -> None:
         while self.run:
@@ -110,23 +183,24 @@ class Game_gomoku:
                         break
                 elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
                     if 0 <= self.mx < self.size and 0 <= self.my < self.size:
-                        if self.chessBoard[self.mx][self.my] == 0 and self.status == "gamerun":
-                            if self.turn == "black":
-                                self.turn = "white"
-                                self.chessBoard[self.mx][self.my] = 1
-                            else:
-                                self.turn = "black"
-                                self.chessBoard[self.mx][self.my] = 2
+                        try:
+                            self.put_chess()
+                        except RuntimeError:
+                            self.temp_text = "position is not empty or game is over"
+                            self.temp_text_time = 60
                     else:
                         mx, my = pygame.mouse.get_pos()
-                        if 60 <= mx < 120 and self.h <= my < self.h + 30:
-                            pygame.quit()
-                            self.run = False
-                            return
-                        elif 0 <= mx < 60 and self.h <= my < self.h + 30:
+                        if 0 <= mx < 60 and self.h <= my < self.h + 30:
                             pygame.quit()
                             self.run = False
                             break
+                        elif 60 <= mx < 120 and self.h <= my < self.h + 30:
+                            pygame.quit()
+                            self.run = False
+                            return
+                        elif 120 <= mx < 180 and self.h <= my < self.h + 30 and self.ai_side == "no":
+                            self.ai_side = self.turn
+                            self.ai()
         self.again()
 
     def again(self) -> None:
@@ -135,5 +209,6 @@ class Game_gomoku:
 
 
 if __name__ == "__main__":
-    game = Game_gomoku(get_int("size(greater than 19 may cause problems): ", "must be great than 5", lambda x: x > 5))
+    size = get_int("size(greater than 19 may cause problems): ", "must be great than 5", lambda x: x > 5)
+    game = Game_gomoku(size)
     game.main()
