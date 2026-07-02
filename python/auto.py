6c4f58dfcd2f8140c154cb63d869a3353faec671
su2u4 <Joey23.su@gmail.com>
2026-07-02 18:45:49 +0800

    update auto.py

--- python/auto.py
+++ python/auto.py
@@ -7,7 +7,8 @@ import pyautogui
 
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = True
-running: bool = False
+running = False
+stop = False
 i1 = (675, 557)
 i2 = (742, 556)
 g1 = (822, 435)
@@ -31,101 +32,96 @@ b6 = (893, 633)
 r1 = (1138, 374)
 
 
-def left(pos: tuple[int, int]) -> None:
+def click(pos: tuple[int, int], button: str) -> None:
+    if stop:
+        raise Exception()
     x, y = pos
     pyautogui.moveTo(x, y)
-    sleep(0.03)
-    pyautogui.mouseDown(button="left")
-    sleep(0.02)
-    pyautogui.mouseUp(button="left")
-    sleep(0.08)
-
-
-def right(pos: tuple[int, int]) -> None:
-    x, y = pos
-    pyautogui.moveTo(x, y)
-    sleep(0.03)
-    pyautogui.mouseDown(button="right")
-    sleep(0.02)
-    pyautogui.mouseUp(button="right")
-    sleep(0.08)
+    sleep(0.01)
+    pyautogui.mouseDown(button=button)
+    sleep(0.01)
+    pyautogui.mouseUp(button=button)
+    sleep(0.01)
 
 
 def put(pos1: tuple[int, int], other_pos: Iterable[tuple[int, int]]) -> None:
     """put pos1 to other_pos"""
-    left(pos1)
+    click(pos1, "left")
     for pos2 in other_pos:
-        right(pos2)
-    left(pos1)
+        click(pos2, "right")
+    click(pos1, "left")
 
 
 def take(pos: tuple[int, int]) -> None:
     """take result(r1) to pos"""
-    left(r1)
-    left(pos)
+    click(r1, "left")
+    click(pos, "left")
 
 
 def run_script() -> None:
     global running
-    print("開始執行")
-    # ===== 在這裡寫你的操作 =====
-    put(i1, [g1])
-    put(i2, [g2, g3, g4])
-    take(a1)
+    try:
+        print("開始執行")
+        # ===== 在這裡寫你的操作 =====
+        put(i1, [g1])
+        put(i2, [g2, g3, g4])
+        take(a1)
 
-    put(i1, [g2])
-    put(i2, [g1, g3, g4])
-    take(a2)
+        put(i1, [g2])
+        put(i2, [g1, g3, g4])
+        take(a2)
 
-    put(i1, [g3])
-    put(i2, [g1, g2, g4])
-    take(a3)
+        put(i1, [g3])
+        put(i2, [g1, g2, g4])
+        take(a3)
 
-    put(i1, [g4])
-    put(i2, [g1, g2, g3])
-    take(a4)
-    # ==========================
-    put(i2, [g1])
-    put(i1, [g2, g3, g4])
-    take(c1)
+        put(i1, [g4])
+        put(i2, [g1, g2, g3])
+        take(a4)
+        # ==========================
+        put(i2, [g1])
+        put(i1, [g2, g3, g4])
+        take(c1)
 
-    put(i2, [g2])
-    put(i1, [g1, g3, g4])
-    take(c2)
+        put(i2, [g2])
+        put(i1, [g1, g3, g4])
+        take(c2)
 
-    put(i2, [g3])
-    put(i1, [g1, g2, g4])
-    take(c3)
+        put(i2, [g3])
+        put(i1, [g1, g2, g4])
+        take(c3)
 
-    put(i2, [g4])
-    put(i1, [g1, g2, g3])
-    take(c4)
-    # ==========================
-    put(i1, [g1, g2])
-    put(i2, [g3, g4])
-    take(b1)
+        put(i2, [g4])
+        put(i1, [g1, g2, g3])
+        take(c4)
+        # ==========================
+        put(i1, [g1, g2])
+        put(i2, [g3, g4])
+        take(b1)
 
-    put(i1, [g2, g4])
-    put(i2, [g1, g3])
-    take(b2)
+        put(i1, [g2, g4])
+        put(i2, [g1, g3])
+        take(b2)
 
-    put(i1, [g3, g4])
-    put(i2, [g1, g2])
-    take(b3)
+        put(i1, [g3, g4])
+        put(i2, [g1, g2])
+        take(b3)
 
-    put(i1, [g1, g3])
-    put(i2, [g2, g4])
-    left(b4)
-    # ==========================
-    put(i1, [g1, g4])
-    put(i2, [g2, g3])
-    take(b5)
+        put(i1, [g1, g3])
+        put(i2, [g2, g4])
+        take(b4)
+        # ==========================
+        put(i1, [g1, g4])
+        put(i2, [g2, g3])
+        take(b5)
 
-    put(i1, [g2, g3])
-    put(i2, [g1, g4])
-    take(b6)
-    # ==========================
-    print("執行完成\n")
+        put(i1, [g2, g3])
+        put(i2, [g1, g4])
+        take(b6)
+        # ==========================
+        print("執行完成\n")
+    except Exception:
+        print("已停止\n")
     running = False
 
 
@@ -134,13 +130,16 @@ def start_script() -> None:
 
 
 def on_press(key: keyboard.Key | keyboard.KeyCode | None) -> None:
-    global running
+    global running, stop
+    if key == keyboard.Key.esc:
+        stop = True
+        return
     if running:
         return
-    if isinstance(key, keyboard.KeyCode):
-        if key.char == "p":
-            running = True
-            start_script()
+    if isinstance(key, keyboard.KeyCode) and key.char == "p":
+        stop = False
+        running = True
+        start_script()
 
 
 print("按 P 開始")
